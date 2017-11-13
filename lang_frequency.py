d618b08036361843af0ec60fc1bc006565a48a1f
bacujiu4 <1984skv@gmail.com>
2017-11-13 18:47:27 +0300

    Валл Ли error fix: magic numbers

--- lang_frequency.py
+++ lang_frequency.py
@@ -19,12 +19,14 @@ def get_most_frequent_words(text):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('file_path')
+    parser.add_argument('most_frequent_words_amount')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
     text = load_data(args.file_path)
+    most_frequent_words_amount = int(args.most_frequent_words_amount)
     most_frequent_words = get_most_frequent_words(text)
-    for word, count in most_frequent_words.most_common(10):
+    for word, count in most_frequent_words.most_common(most_frequent_words_amount):
         print('{0:3} - {1:0d}'.format(word, count))
